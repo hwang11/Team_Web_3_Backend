b34ffc4955233e22e991b4a3ecd33a8dc52b066f
황선아 <tjsdk2769@gmail.com>
2020-05-17 23:00:35 +0900

    Add authController

---
+++ src/main/java/com/web/yapp/server/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.web.yapp.server.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.security.Principal;
+@CrossOrigin("*")
+@Api(tags = "인증체크 API")
+@RestController
+public class AuthController {
+
+    @Autowired
+    FindByIndexNameSessionRepository<? extends Session> sessions;
+
+    @ApiOperation(value = "로그인 유저 데이터 확인")
+    @GetMapping("/auth")
+    public Principal home(Principal principal) {
+        return principal;
+    }
+
+    @ApiOperation(value = "로그인 유저 세션 확인")
+    @GetMapping("/session")
+    public String currentSession(){
+        if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() == "anonymousUser")
+            return "ANONYMOUS";
+
+        Session findSession = sessions.getSession(RequestContextHolder.currentRequestAttributes().getSessionId());
+        if(findSession != null)
+            return findSession.getId();
+
+        return "INVALID";
+    }
